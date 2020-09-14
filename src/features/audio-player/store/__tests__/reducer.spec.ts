875dd475b7e0923505d6854be86178e17551ece2
Ayub-Begimkulov <begimkulov963@gmail.com>
2020-09-14 11:15:29 +0300

    add reducer test

--- src/features/audio-player/store/__tests__/reducer.spec.ts
+++ src/features/audio-player/store/__tests__/reducer.spec.ts
@@ -1,6 +1,17 @@
 import { audioPlayerReducer } from '..';
 import { reducerSanityCheck } from 'test-utils';
+import { setPlayingRecord } from '../actions';
+import fakeRecords from 'features/call-records/fixtures/call-records';
 
 describe('audioPlayerReducer', () => {
   reducerSanityCheck(audioPlayerReducer);
+
+  it('setPlayingRecord', () => {
+    const record = fakeRecords[0];
+    const actual = { playingRecord: null };
+    const expected = { playingRecord: record };
+    expect(audioPlayerReducer(actual, setPlayingRecord(record))).toStrictEqual(
+      expected
+    );
+  });
 });
