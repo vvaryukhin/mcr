a9133e45edbb1b430d524d2d053a1c41d32f0c24
Ayub-Begimkulov <begimkulov963@gmail.com>
2020-10-20 10:26:57 +0300

    fix homepage

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 
-export const history = createBrowserHistory();
+export const history = createHashHistory();
 
 const BrowserRouter: React.FC = ({ children }) => {
   return <Router history={history}>{children}</Router>;
